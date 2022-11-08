e7df7161bd87eb61dd92131ac868025ede82fda2
Filip Kępka <s188963@o365.student.pg.edu.pl>
2022-11-08 10:48:29 +0100

    add bounds

--- src/Coin.h
+++ src/Coin.h
@@ -1,11 +1,24 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+#include <SFML/System.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Network.hpp>
+
 class Coin
 {
+private:
+    sf::CircleShape shape;
+
+    void initShape();
+
 public:
     Coin();
-    ~Coin();
+    virtual ~Coin();
 
-private:
+    // Functions
+    void update();
+    void render(sf::RenderTarget* target);
 
 };
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,3 +1,4 @@
+#include <bits/stdc++.h>
 #include "Player.h"
 
 void Player::initVariables()
@@ -41,12 +42,34 @@ void Player::updateInput()
         this->shape.move(0.f, this->movementSpeed);
 }
 
-void Player::update(sf::RenderTarget* target)
+void Player::updateWindowBoundsCollision(const sf::RenderTarget* target)
 {
-   // Window bounds collision
-    
 
-   this->updateInput();
+    // Left
+    if (this->shape.getGlobalBounds().left <= 0.f)
+        this->shape.setPosition(0.f, this->shape.getGlobalBounds().top);
+
+    // Right
+    if (this->shape.getGlobalBounds().left + this->shape.getGlobalBounds().width >= target->getSize().x)
+        this->shape.setPosition(target->getSize().x - this->shape.getGlobalBounds().width, this->shape.getGlobalBounds().top);
+
+    // Top
+    if (this->shape.getGlobalBounds().top <= 0.f)
+        this->shape.setPosition(this->shape.getGlobalBounds().left, 0.f);
+
+    // Bottom
+    if (this->shape.getGlobalBounds().top + this->shape.getGlobalBounds().height >= target->getSize().y)
+        this->shape.setPosition(this->shape.getGlobalBounds().left, target->getSize().y - this->shape.getGlobalBounds().height);
+
+}
+
+void Player::update(const sf::RenderTarget* target)
+{
+    // Keyboard input
+    this->updateInput();
+
+   // Window bounds collision
+    this->updateWindowBoundsCollision(target);
 }
 
 void Player::render(sf::RenderTarget* target)
